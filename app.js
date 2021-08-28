d1739cd3298347128a58375682b8f3daeb29a98d
JmA515 <janderson515@yahoo.com>
2021-08-28 13:46:08 -0700

    Restructured prompts for confirming selections.

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
 "use strict"
 
 let destinations = ["NYC", "Paris", "New Zealand", "San Diego", "Spain"];
-let restaurants = ["Fine Dining", "a Classic Dinner", "a Food Truck", "Fast Food", "Packed Lunch"];
-let modeOfTransportation = ["Walking", "a Taxi", "a Bicycle", "a Sccoter", "a Trolley"];
-let formOfEntertainment = ["Sight Seeing", "a Museum", "the Zoo", "a City Park", "Amusement Park"];
+let restaurants = ["Fine Dining", "a Classic Dinner", "a Food Truck", 
+"Fast Food", "Packed Lunch"];
+let modeOfTransportation = ["Walking", "a Taxi", "a Bicycle", 
+"a Scooter", "a Trolley"];
+let formOfEntertainment = ["Sight Seeing", "a Museum", "the Zoo", 
+"a City Park", "Amusement Park"];
 
 function randomItems(arrayOfThings){
     return arrayOfThings[Math.floor(Math.random()*arrayOfThings.length)];
 }
 
-let place = randomItems(destinations);
-console.log(place);
+let userInput;
+let userInput2;
+let userInput3;
+let userInput4;
 
-let typeOfFood = randomItems(restaurants);
-console.log(typeOfFood);
+let place;
+let typeOfFood;
+let typeOfTransportation;
+let funThingsToDo;
 
-let typeOfTransportation = randomItems(modeOfTransportation);
-console.log(typeOfTransportation);
+while(userInput != "yes"){
+    place = randomItems(destinations);
+    userInput = prompt(`Would you like to go to ${place}? Yes or no.`);
+}
 
-let funThingsToDo = randomItems(formOfEntertainment);
-console.log(funThingsToDo);
+while(userInput2 != "yes"){
+    typeOfFood = randomItems(restaurants);
+    userInput2 = prompt(`Would you like to go to ${typeOfFood}? Yes or no.`);
+}
 
-let userInput = prompt("You're destination is" + place + "." +
- "You're type of dining is" + typeOfFood + "." +
- "You're mode of transportation will be" + typeOfTransportation + "." +
- "And you will be enjoying" + funThingsToDo + "." +
- "Are you happy with the current selection? Yes or No.");
+while(userInput3 != "yes"){
+    typeOfTransportation = randomItems(modeOfTransportation);
+    userInput3 = prompt(`Would you like to go by ${typeOfTransportation}? Yes or no.`);
+}
+
+while(userInput4 != "yes"){
+    funThingsToDo = randomItems(formOfEntertainment);
+    userInput4 = prompt(`Would you like to go ${funThingsToDo}? Yes or no.`);
+}
+
